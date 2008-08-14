45983aaded9a7096331dbf4c71ebe61d612297f2
kortmann <kortmann@1d6cf3d0-5503-4355-b6c5-b92237d31720>
2008-08-14 07:25:18 +0000

    First checkin of the stripped down standalone version. Source needs some cleanup and documentation

---
+++ buildit.m
@@ -0,0 +1,23 @@
+fprintf ('compiling mksqlite...\n');
+
+mksqlite_compile_currdir = pwd;
+cd (fileparts(mfilename('fullpath')));
+
+mksqlite_compile_subwcrev = [getenv('ProgramFiles') '\TortoiseSVN\bin\SubWCRev.exe'];
+
+if exist(mksqlite_compile_subwcrev, 'file')
+    fprintf ('svn revision info:\n');
+    system(['"' mksqlite_compile_subwcrev '" ' pwd ' svn_revision.tmpl svn_revision.h']);
+else
+    if ~exist('svn_revision.h','file')
+        copyfile('svn_revision.dummy','svn_revision.h');
+    end
+end
+mex -output mksqlite -DNDEBUG#1 -O mksqlite.cpp sqlite3.c user32.lib advapi32.lib
+
+cd (mksqlite_compile_currdir);
+
+clear mksqlite_compile_currdir;
+clear mksqlite_compile_subwcrev;
+
+fprintf ('completed.\n');
