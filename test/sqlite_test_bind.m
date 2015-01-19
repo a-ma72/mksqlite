b9b738cf8eee25eda8e8ea461be421f44861f01f
andimartin <andimartin@1d6cf3d0-5503-4355-b6c5-b92237d31720>
2015-01-19 12:55:37 +0000

    Examples review and documentation Fixes: - Ability to switch between language at runtime - 3rd result of mksqlite has now 2 columns (field name and SQL name) - Less arguments than needed for parameter binding allowed again (param_wrapping must be "off")

--- test/sqlite_test_bind.m
+++ test/sqlite_test_bind.m
@@ -1,82 +1,86 @@
 function sqlite_test_bind
 
-  clear all
-  close all
-  clc
-  mksqlite('version mex');
-  
-  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-  % Create database with some records  %
-  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-  
-  fprintf( 'Creating in-memory database...\n' );
-  mksqlite( 'open', ':memory:' ); % "in-memory"-database
-  
-  %          |First name |Last name    |City         |Random data
-  mydata = { ...
-             'Gunther',  'Meyer',      'München',    []; ...
-             'Holger',   'Michelmann', 'Garbsen',    rand( 1, 10 ); ...
-             'Knuth',    'Almeroth',   'Wehnsen',    'Arbeitskollege' ...
-           }; 
-  
-  % Create table
-  mksqlite( 'create table demo (Col_1, Col_2, Col_3, Data)' );
-  
-  % Create records
-  for i = 1:size( mydata, 1 )
-    mksqlite( 'insert into demo values (?,?,?,?)', mydata{i,:} );
-  end
+    clear all
+    close all
+    clc
+    dummy = mksqlite('version mex');
+    fprintf( '\n\n' );
 
-  % Take screenshot (figure) as RGB-matrix...
-  h = figure;
-  set( h, 'units', 'normalized', 'position', [0.5,0.5,0.2,0.2] );
-  x = linspace(0,2*pi,20);
-  plot( x, sin(x), 'r-', 'linewidth', 2 );
-  legend on
-  F = getframe(h);
-  delete(h);
-  data = F.cdata;
-  
-  % ... write back as BLOB
-  mksqlite( 'insert into demo values (?,?,?,?)', ...
-            size( data, 1 ), size( data, 2 ), size( data, 3 ), data );
-  
-          
-          
-  %%%%%%%%%%%%%%%%%
-  % Read database %
-  %%%%%%%%%%%%%%%%%
-  
-  fprintf( 'Restore BLOB records...\n\n' )
-  
-  query = mksqlite( 'select * from demo' );
-  
-  fprintf( '---> Empty array: ' ), ...
-           query(1).Data
-         
-  fprintf( '---> 10 random numbers between 0 and 1: ' ), ...
-           typecast( query(2).Data, 'double' )
-         
-  fprintf( '---> Text: ' ), ...
-           cast( query(3).Data, 'char' )
-         
-  fprintf( '---> Image: (see figure) \n\n' )
-  
-  % Reshape vector as multidimensional array (RGB matrix):
-  img = reshape( query(4).Data, query(4).Col_1, query(4).Col_2, query(4).Col_3 );
-  
-  h = image( img );
-  axis off
-  set( gcf, 'units', 'normalized', 'position', [0.5,0.5,0.2,0.2] );
-  drawnow
-  
-  try
-    warning( 'off', 'MATLAB:HandleGraphics:ObsoletedProperty:JavaFrame' );
-    jh = get( h, 'JavaFrame' );
-    jh.fFigureClient.getWindow.setAlwaysOnTop( true );
-    jh.fFigureClient.getWindow.setVisible( true );
-  catch
-  end
-  
-  mksqlite( 'close' );
-end
+    
+    % create a database with some records
+    fprintf( 'Creating in-memory database...\n' );
+    mksqlite( 'open', ':memory:' ); % "in-memory"-database
+
+    %          |First name |Last name    |City         |Random data
+    mydata = { ...
+               'Gunther',  'Meyer',      'Munich',     []; ...
+               'Holger',   'Michelmann', 'Garbsen',    rand( 1, 10 ); ...
+               'Knuth',    'Almeroth',   'Wehnsen',    'coworker' ...
+             }; 
+
+    % create table
+    mksqlite( 'CREATE TABLE demo (Col_1, Col_2, Col_3, Data)' );
+
+    % create records
+    % uses "cell expansion" for command shortening!
+    for i = 1:size( mydata, 1 )
+        mksqlite( 'INSERT INTO demo VALUES (?,?,?,?)', mydata{i,:} );
+    end
+
+    % take a screenshot (figure) as RGB-matrix...
+    h = figure;
+    set( h, 'units', 'normalized', 'position', [0.5,0.5,0.2,0.2] );
+    x = linspace( 0, 2*pi, 20 );
+    plot( x, sin(x), 'r-', 'linewidth', 2 );
+    legend on
+    F = getframe(h);
+    delete(h);
+    data = F.cdata;
+
+    % ... write back screenshot information as BLOB with additional 
+    % array size parameters
+    mksqlite( 'INSERT INTO demo VALUES (?,?,?,?)', ...
+              size( data, 1 ), size( data, 2 ), size( data, 3 ), data );
+
+
+
+    
+    % ------------------------------------------------------------------
+
+    
+    % read back all records
+    fprintf( 'Restore BLOB records...\n\n' )
+
+    query = mksqlite( 'SELECT * FROM demo' );
+
+    fprintf( '---> Empty array: ' ), ...
+             query(1).Data
+
+    fprintf( '---> 10 random numbers between 0 and 1: ' ), ...
+             typecast( query(2).Data, 'double' )
+
+    fprintf( '---> Text: ' ), ...
+             cast( query(3).Data, 'char' )
+
+    fprintf( '---> Image: (see figure) \n\n' )
+
+    % Reshape vector from 4th row as multidimensional array (RGB matrix) back:
+    img = reshape( query(4).Data, query(4).Col_1, query(4).Col_2, query(4).Col_3 );
+
+    % display image got from database
+    h = image( img );
+    axis off
+    set( gcf, 'units', 'normalized', 'position', [0.5,0.5,0.2,0.2] );
+    drawnow
+
+    % bring figure on top
+    try
+        warning( 'off', 'MATLAB:HandleGraphics:ObsoletedProperty:JavaFrame' );
+        jh = get( h, 'JavaFrame' );
+        jh.fFigureClient.getWindow.setAlwaysOnTop( true );
+        jh.fFigureClient.getWindow.setVisible( true );
+    catch
+        % ignore errors
+    end
+
+    mksqlite( 'close' );
--- test/sqlite_test_load_extension.m
+++ test/sqlite_test_load_extension.m
@@ -1,22 +1,27 @@
 function sqlite_test_load_extension
-  % Create in-memory database
-  dbid = mksqlite( 'open', '' ); 
-  
-  % SpatiaLite V3.0.1
-  % (ref: http://www.gaia-gis.it/gaia-sins/windows-bin-x86-prev/spatialite-3.0.1-DLL-win-x86.zip)
-  
-  % Enable SQLite extensions
-  mksqlite( dbid, 'enable extension', 1 );
-  
-  % Bind SpatiaLite
-  q = mksqlite( dbid, 'select load_extension("spatialite.dll")' );
-  
-  if ~isstruct(q)
-    error( 'Error while binding SpatiaLite DLL' );
-  end
-  
-  fprintf( 'SpatiaLite version:' );
-  mksqlite( dbid, 'select spatialite_version() as Version' )
-  
-  mksqlite( 0, 'close' ); % Close all databases
-end
+    
+    if length(dbstack) ~= 1
+        return
+    end
+    
+    % Create in-memory database
+    dbid = mksqlite( 'open', '' ); 
+
+    % SpatiaLite V3.0.1
+    % (ref: http://www.gaia-gis.it/gaia-sins/windows-bin-x86-prev/spatialite-3.0.1-DLL-win-x86.zip)
+
+    % Enable SQLite extensions
+    mksqlite( dbid, 'enable extension', 1 );
+
+    % Bind SpatiaLite
+    q = mksqlite( dbid, 'SELECT load_extension("libspatialite-2.dll")' );
+
+    if ~isstruct(q)
+      error( 'Error while binding SpatiaLite DLL' );
+    end
+
+    fprintf( 'SpatiaLite version:' );
+    mksqlite( dbid, 'SELECT spatialite_version() AS Version' )
+
+    mksqlite( 'close' ); % Close database
+
