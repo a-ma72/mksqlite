7505642b347dee5f0f1e184bd62d83970a3b9276
AndreasMartin72 <andi.martin@freenet.de>
2021-12-20 15:55:12 +0100

    Documentation update (Doxygen)

--- py/filter.py
+++ py/filter.py
@@ -1,35 +1,33 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 
 def filter_lines():
-    f = file(sys.argv[1],'r')
-    iHide = 0
-    replacement = '' #'\n'
-    
-    for line in f:
-        if line.find('\\hide') >= 0:
-            iHide = iHide - 1
-            sys.stdout.write(replacement)
-            continue
+    with open( sys.argv[1], 'rt' ) as f:
+        iHide = 0
+        replacement = '' #'\n'
+        
+        for line in f:
+            if line.find( r'\hide' ) >= 0:
+                iHide = iHide - 1
+                sys.stdout.write( replacement )
+                continue
 
-        if line.find('\\endhide') > 0 and iHide < 0:
-            iHide += 1
-            sys.stdout.write(replacement)
-            continue
+            if line.find( r'\endhide' ) > 0 and iHide < 0:
+                iHide += 1
+                sys.stdout.write( replacement )
+                continue
 
-        if line.find('\\stophide') > 0:
-            iHide = 0
-            sys.stdout.write(replacement)
-            continue
+            if line.find( r'\stophide' ) > 0:
+                iHide = 0
+                sys.stdout.write( replacement )
+                continue
 
-        if iHide >= 0:
-            sys.stdout.write(line)
-        else:
-            sys.stdout.write(replacement)
+            if iHide >= 0:
+                sys.stdout.write( line )
+            else:
+                sys.stdout.write( replacement )
             
-    f.close()
-
 
 filter_lines()
 
